a0bf59098a3b085ef854220ca90e942a319f8053
tsformable <tsformable@gmail.com>
2023-08-01 11:48:15 -0400

    Add: Layout

---
+++ src/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header:React.FC = () => {
+      return (
+            <>
+               <div className=" bg-slate-500 p-8">
+                  <h1 className="text-3xl text-white">Loan-Lending</h1>
+               </div>
+            </>
+      );
+}
+
+export default Header;
